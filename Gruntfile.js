783315407282d05754e46a15611e565ee8aa8927
Joscha Feth <joscha@feth.com>
2014-11-20 03:06:02 +0100

    fix config

--- Gruntfile.js
+++ Gruntfile.js
@@ -32,12 +32,12 @@ module.exports = function(grunt) {
         updateConfigs: ['pkg'],
         commit: true,
         commitMessage: 'Release v%VERSION%',
-        commitFiles: ['package.json', 'bower.json'],
+        commitFiles: ['package.json', 'bower.json', 'build/**'],
         createTag: true,
         tagName: 'v%VERSION%',
         tagMessage: 'Version %VERSION%',
         push: true,
-        pushTo: 'upstream',
+        pushTo: 'origin',
         gitDescribeOptions: '--tags --always --abbrev=1 --dirty=-d',
         globalReplace: false
       }
